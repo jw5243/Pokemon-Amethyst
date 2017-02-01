8cf2e89537c2db2dce6fbb8f0cd3db032e4217e2
jw5243 <jtwong71@gmail.com>
2017-01-31 22:33:37 -0500

    PackedBuilder can append Strings properly.

--- core/src/com/horse/utility/PackedStrings/MutablePacked.java
+++ core/src/com/horse/utility/PackedStrings/MutablePacked.java
@@ -25,19 +25,17 @@ public class MutablePacked extends PackedBase {
         }
         final float quarterArLength     = ar.length / 4f;
         final int   arraySizeAllocation = quarterArLength % 1f != 0 ? (int)(quarterArLength) + 1 : (int)(quarterArLength);
-        final int   originalSize        = (getStringStorage() != null ? getStringStorage().length : 0);
+        final int   originalSize        = getStringStorage() != null ? getStringStorage().length : 0;
         int[]       stringStorage       = new int[arraySizeAllocation + originalSize];
         for(int index = 0; index < stringStorage.length; index++) {
             if(getStringStorage() != null && index < getStringStorage().length) {
                 stringStorage[index] = getStringStorage()[index];
             } else {
-                int quadrupleIndex = index << 2;
-                if(getStringStorage() == null) {
-                    stringStorage[index] = get(ar, quadrupleIndex + 3) | get(ar, quadrupleIndex + 2) << 8 | get(ar, quadrupleIndex + 1) << 16 | get(ar, quadrupleIndex) << 24;
-                } else {
-                    stringStorage[index] = get(ar, quadrupleIndex + 3 - getStringStorage().length) | get(ar, quadrupleIndex + 2 - getStringStorage().length) << 8 |
-                                           get(ar, quadrupleIndex + 1 - getStringStorage().length) << 16 | get(ar, quadrupleIndex - getStringStorage().length) << 24;
-                }
+                final int quadrupleIndex      = index << 2;
+                final int stringStorageLength = getStringStorage() != null ? getStringStorage().length << 2 : 0;
+                stringStorage[index] =
+                        get(ar, quadrupleIndex + 3 - stringStorageLength) | get(ar, quadrupleIndex + 2 - stringStorageLength) << 8 | get(ar, quadrupleIndex + 1 - stringStorageLength) << 16 |
+                        get(ar, quadrupleIndex - stringStorageLength) << 24;
             }
         }
         setStringStorage(stringStorage);
